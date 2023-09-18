943156fd10621acb7a9a12f845aff5a876f2637a
Abhishek232110 <abhi7376862392@gmail.com>
2023-09-19 00:37:34 +0530

    change

--- src/cloneOfInternship/prefrenceIntern/index.jsx
+++ src/cloneOfInternship/prefrenceIntern/index.jsx
@@ -4,9 +4,15 @@ export default function PreferenceIndex() {
   return (
     <>
       <div className="bg-green-100">
-        <h1 className="text-center my-10 text-2xl font-semibold pt-5">
+        <h1 className="text-center mt-10  text-3xl  font-semibold pt-5">
           Internship
         </h1>
+        <div className="flex items-center justify-center py-2">
+          <h1> as per your</h1>
+          <a href="" className="pl-1 text-blue-600">
+            preference
+          </a>
+        </div>
         <div className="flex my-2  ">
           <div className="w-36 "></div>
           <div className="  overflow-auto py-3">
