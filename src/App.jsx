5bd65991cb3a997849e09e38acc4d8ca6734060d
Abhishek232110 <abhi7376862392@gmail.com>
2023-09-21 14:48:31 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import AppRoutes from "./app/routers";
-import UserIndex from "./internProject/userPage";
 
 function App() {
   return (
     <>
       <div className="">
-        <UserIndex />
+        <AppRoutes />
       </div>
     </>
   );
