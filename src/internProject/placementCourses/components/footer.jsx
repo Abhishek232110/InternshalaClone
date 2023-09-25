56335c411dfb3503053832f469bbd81f427520d9
sushil <sushil88081@gmail.com>
2023-09-25 20:42:33 +0530

    create-footer

--- src/internProject/placementCourses/components/footer.jsx
+++ src/internProject/placementCourses/components/footer.jsx
@@ -1,101 +1,135 @@
-import React from "react";
-export function Link({ d }) {
+import { Table } from "@mui/material";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData(
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "Internship in Delhi",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "Internship in Banglore",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "Internship in Mumbai",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "Internship in Hydrabad",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "Internship in Chennai",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "Internship in Gurgaon",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "Internship in Kolkata",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "Virtual internship",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+  createData(
+    "View all internships",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India",
+    "Internship in India"
+  ),
+];
+
+export default function Footer() {
+  const d = new Date();
+  let year = d.getFullYear();
   return (
-    <div className="flex justify-around mx-4">
-      <a href="#">{d.link1}</a>
-      <a href="#">{d.link2}</a>
-      <a href="#">{d.link3}</a>
-      <a href="#">{d.link4}</a>
-      <a href="#">{d.link5}</a>
-      <a href="#">{d.link6}</a>
-      <a href="#">{d.link7}</a>
+    <div className="bg-gray-700 p-4 ">
+      <footer>
+        <Table>
+          <thead>
+            <tr className="bg-gray-700  text-white">
+              <th className="bg-gray-700 text-white">Internship by places</th>
+              <th className="bg-gray-700 text-white">Internship by Stream</th>
+              <th className="bg-gray-700 text-white">Jobs by Places</th>
+              <th className="bg-gray-700 text-white">Jobs by Stream</th>
+              <th className="bg-gray-700 text-white">Online Trainings</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((row) => (
+              <tr key={row.name} className="bg-gray-700">
+                <td className="bg-gray-700 text-white">
+                  <a href="#">{row.name}</a>
+                </td>
+                <td className="bg-gray-700 text-white">
+                  <a href="#">{row.calories}</a>
+                </td>
+                <td className="bg-gray-700 text-white">
+                  <a href="#">{row.fat}</a>
+                </td>
+                <td className="bg-gray-700 text-white">
+                  <a href="#">{row.carbs}</a>
+                </td>
+                <td className="bg-gray-700 text-white">
+                  <a href="#">{row.protein}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </footer>
+      <hr className="my-5 bg-white" />
+
+      <div className="flex justify-end py-5 text-white">
+        &copy; {year} Your Company. All rights reserved.
+      </div>
     </div>
   );
 }
-const FooterPage = () => {
-  const d = new Date();
-  let year = d.getFullYear();
-  // Define the data for the footer
-
-  const data = [
-    {
-      id: 1,
-      title: "Internship by places",
-      link1: "Internship in India",
-      link2: "Internship in Delhi",
-      link3: "Internship in Banglore",
-      link4: "Internship in Mumbai",
-      link5: "Internship in Hydrabad",
-      link6: "Internship in India",
-      link7: "Internship in Delhi",
-    },
-    {
-      id: 2,
-      title: "Internship by Stream",
-      link1: "Internship in India",
-      link2: "Internship in Delhi",
-      link3: "Internship in Banglore",
-      link4: "Internship in Mumbai",
-      link5: "Internship in Hydrabad",
-      link6: "Internship in India",
-      link7: "Internship in Delhi",
-    },
-    {
-      id: 3,
-      title: "Jobs by Places",
-      link1: "Internship in India",
-      link2: "Internship in Delhi",
-      link3: "Internship in Banglore",
-      link4: "Internship in Mumbai",
-      link5: "Internship in Hydrabad",
-      link6: "Internship in India",
-      link7: "Internship in Delhi",
-    },
-    {
-      id: 4,
-      title: "Jobs by Stream",
-      link1: "Internship in India",
-      link2: "Internship in Delhi",
-      link3: "Internship in Banglore",
-      link4: "Internship in Mumbai",
-      link5: "Internship in Hydrabad",
-      link6: "Internship in India",
-      link7: "Internship in Delhi",
-    },
-    {
-      id: 5,
-      title: "Online Trainings",
-      link1: "Internship in India",
-      link2: "Internship in Delhi",
-      link3: "Internship in Banglore",
-      link4: "Internship in Mumbai",
-      link5: "Internship in Hydrabad",
-      link6: "Internship in India",
-      link7: "Internship in Delhi",
-    },
-  ];
-
-  return (
-    <>
-      <footer className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto ">
-          {data.map((d) => {
-            return (
-              <>
-                {/* <div>{d.title}</div> */}
-
-                <Link d={d} />
-              </>
-            );
-          })}
-        </div>
-        <hr className="my-5" />
-        <div className="flex justify-end py-5">
-          &copy; {year} Your Company. All rights reserved.
-        </div>
-      </footer>
-    </>
-  );
-};
-
-export default FooterPage;
